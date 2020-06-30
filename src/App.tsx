9d624aff82c37aa168e55c57fab7a05eae254b0c
Gabriel Asakawa <gabriel.asakawa@gmail.com>
2020-06-29 23:04:38 -0300

    feat: finish the routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,11 @@
 import React from 'react';
+import { BrowserRouter } from 'react-router-dom';
+import Routes from './routes';
 
-const App: React.FC = () => {
-  return (
-    <div className="App">
-      <h1>Hello World</h1>
-    </div>
-  );
-};
+const App: React.FC = () => (
+  <BrowserRouter>
+    <Routes />
+  </BrowserRouter>
+);
 
 export default App;
---
+++ src/routes/index.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+import Dashboard from './pages/Dashboard';
+import Repository from './pages/Repository';
+
+const Routes: React.FC = () => (
+  <Switch>
+    <Route component={Dashboard} path="/" exact />
+    <Route component={Repository} path="/repository" />
+  </Switch>
+);
+
+export default Routes;
